4552ac49f525d958f5e7143cb41d6c684b7ce090
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2026-04-28 00:48:06 +0530

    Add: search-for-word-ii - submission-0

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-0.java
@@ -0,0 +1,62 @@
+class Solution {
+
+    static class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        String word;
+    }
+
+    public java.util.List<String> findWords(char[][] board, String[] words) {
+        TrieNode root = buildTrie(words);
+        java.util.List<String> result = new java.util.ArrayList<>();
+
+        int m = board.length, n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dfs(board, i, j, root, result);
+            }
+        }
+
+        return result;
+    }
+
+    private void dfs(char[][] board, int i, int j, TrieNode node, java.util.List<String> result) {
+        char c = board[i][j];
+
+        if (c == '#' || node.children[c - 'a'] == null) return;
+
+        node = node.children[c - 'a'];
+
+        if (node.word != null) {
+            result.add(node.word);
+            node.word = null;
+        }
+
+        board[i][j] = '#';
+
+        if (i > 0) dfs(board, i - 1, j, node, result);
+        if (j > 0) dfs(board, i, j - 1, node, result);
+        if (i < board.length - 1) dfs(board, i + 1, j, node, result);
+        if (j < board[0].length - 1) dfs(board, i, j + 1, node, result);
+
+        board[i][j] = c;
+    }
+
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+
+        for (String word : words) {
+            TrieNode node = root;
+            for (char c : word.toCharArray()) {
+                int idx = c - 'a';
+                if (node.children[idx] == null) {
+                    node.children[idx] = new TrieNode();
+                }
+                node = node.children[idx];
+            }
+            node.word = word;
+        }
+
+        return root;
+    }
+}
