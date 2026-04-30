280dce77714c18088f4072e9d64b6eb61b094b22
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2026-05-01 00:30:49 +0530

    Add: sum-of-two-integers - submission-0

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int carry = (a & b) << 1; // carry
+            a = a ^ b;                // sum without carry
+            b = carry;               // add carry next
+        }
+        return a;
+    }
+}
